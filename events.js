ec02af903d21f2e3a1664e01200819bcc59d9de4
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-11 11:50:05 -0600

    addded argument to emitter callback funtion

--- events.js
+++ events.js
@@ -1,16 +1,20 @@
-
-
 const EventEmitter = require('events');
 const celebrity = new EventEmitter();
 
 
+//call back functions
+celebrity.on('race', (result => {
+        if (result === 'win') {
+            console.log("you win");
+        }
+    })
+);
 
-celebrity.on('race win', () => {
-    console.log("you win");
-})
-
-celebrity.on('race win', () => {
-    console.log("awww");
-})
-celebrity.emit('race win');
+celebrity.on('race', (result) => {
+    if (result === "loss") {
+        console.log("awww");
+    }
+});
+celebrity.emit('race', 'win');
+celebrity.emit('race', 'loss');
 
